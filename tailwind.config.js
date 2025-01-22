2f28ea0c9d4ed31c84ba115dd82110da1bb55324
pinje <melvinaustinn@gmail.com>
2025-01-23 02:32:57 +0700

    Adding container, color, and screens config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'selector',
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       backgroundImage: {
         parallax: "url('/images/f1c3fc727775a7378b32c4596e2e0ca2.gif')",
+      },
+      colors: {
+        // primary: '#78716c',
+        primary: '#41B883',
+        secondary: '#94a3b8',
+        dark: '#121212',
+      },
+      screens: {
+        '2xl': '1320px',
       },
     },
   },
