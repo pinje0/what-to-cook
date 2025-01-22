006091390c25f1297528efcc36764a6deb2c3b3c
pinje <melvinaustinn@gmail.com>
2025-01-23 02:41:00 +0700

    change folder pages & components to lower-case

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 // import Recommendation from './pages/Recommendation';
-import Footer from './Components/Footer';
-import About from './Pages/About';
+import Footer from './components/Footer';
+import About from './pages/About';
 
 const App = () => {
   return (
