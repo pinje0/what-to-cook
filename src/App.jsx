abd4c1cf48de9414b016daacdf48b96d0a773b29
pinje <melvinaustinn@gmail.com>
2025-01-23 02:30:38 +0700

    refactoring home & about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Recommendation from './pages/Recommendation';
-// import Footer from './components/Footer';
-import About from './pages/About';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+// import Recommendation from './pages/Recommendation';
+import Footer from './Components/Footer';
+import About from './Pages/About';
 
 const App = () => {
   return (
@@ -11,11 +11,11 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recommendation" element={<Recommendation />} />
-        <Route path="/favorite" element={<div>adasdas</div>} />
+        {/* <Route path="/recommendation" element={<Recommendation />} /> */}
+        {/* <Route path="/favorite" element={<div>adasdas</div>} /> */}
         <Route path="/about" element={<About />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 };
