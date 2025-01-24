c1edad6c0e4c8924189fd1a01255c1b6de1bc309
pinje <melvinaustinn@gmail.com>
2025-01-25 02:00:52 +0700

    Improve: Recommendation, DetailRecipe, Favorite, and About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-// import Recommendation from './pages/Recommendation';
 import Footer from './components/Footer';
 import About from './pages/About';
+import Recommendation from './pages/Recommendation';
+import RecipeDetail from './pages/RecipeDetail';
+import Favorite from './pages/Favorite';
 
 const App = () => {
   return (
@@ -11,8 +13,9 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/recommendation" element={<Recommendation />} /> */}
-        {/* <Route path="/favorite" element={<div>adasdas</div>} /> */}
+        <Route path="/recommendation" element={<Recommendation />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/favorite" element={<Favorite />} />
         <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
