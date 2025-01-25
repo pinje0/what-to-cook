7017170783b01f8b2c1beb70a4905987f132f0eb
pinje <melvinaustinn@gmail.com>
2025-01-26 01:11:32 +0700

    Improve: Hover button add/remove color

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -94,7 +94,7 @@ const RecipeDetail = () => {
         <button
           onClick={toggleFavorite}
           className={`mt-4 px-6 py-2 button-74 rounded-lg shadow-md transition duration-500 ${
-            isFavorite ? 'hover:bg-red-500' : 'hover:bg-green-500'
+            isFavorite ? 'hover:bg-red-500 hover:text-white' : 'hover:bg-green-500 hover:text-white'
           }`}
         >
           {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
